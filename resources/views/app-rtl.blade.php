526bac68dc1fe1aa5927dce51b6e3a1bdd1a5ff3
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-02-15 11:42:07 +0100

    fix dev to production

--- resources/views/app-rtl.blade.php
+++ resources/views/app-rtl.blade.php
@@ -47,11 +47,11 @@
 <script src="js/aside.js"></script>
 
 
-        -->
 
 @if(config('app.env') == 'local')
     <script src="http://localhost:35729/livereload.js"></script>
 @endif
 
+        -->
 </body>
 </html>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,9 +39,5 @@
 <script src="{{ mix('js/vendor.js') }}"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 
-@if(config('app.env') == 'local')
-    <script src="http://localhost:35729/livereload.js"></script>
-@endif
-
 </body>
 </html>
